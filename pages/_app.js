d584a584f783f4a423d72a0e672027435c2c7d37
Snehil <snehilwebber@gmail.com>
2020-11-11 05:08:29 +0530

    feat(*): add footer with basic meta

--- pages/_app.js
+++ pages/_app.js
@@ -85,6 +85,39 @@ function MyApp({ Component, pageProps, router }) {
         </div>
       </nav>
       <Component {...pageProps} />
+      <footer>
+        <span>Contact Us</span>
+        <section>
+          <article>
+            <svg
+              className="w-6 h-6"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
+                clipRule="evenodd"
+              />
+            </svg>
+            UB-703, SRMIST, KTR - 603203
+          </article>
+          <article>
+            <svg
+              className="w-6 h-6"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+              <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+            </svg>
+            info@nextech.io
+          </article>
+        </section>
+        Made with ❤️️ by Snehil
+      </footer>
     </>
   );
 }
